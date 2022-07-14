9e274e89ac4ee91eea21596d5445315dfa297f79
Ejay Torres <ejay.torres@brank.as>
2022-07-14 14:02:38 +0800

    Updated Balance Tap Version to 1.1.0

--- app/src/main/java/com/brankas/testapp/adapter/BalanceBanksAdapter.kt
+++ app/src/main/java/com/brankas/testapp/adapter/BalanceBanksAdapter.kt
@@ -1,23 +1,13 @@
 package com.brankas.testapp.adapter
 
 import android.content.Context
-import android.graphics.drawable.PictureDrawable
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.brankas.testapp.R
-import com.brankas.testapp.custom.SvgDecoder
-import com.brankas.testapp.custom.SvgDrawableTranscoder
-import com.brankas.testapp.custom.SvgSoftwareLayerSetter
 import com.brankas.testapp.databinding.ItemBalanceBankBinding
 import com.brankas.testapp.viewmodel.BalanceBankItemViewModel
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.model.StreamEncoder
-import com.bumptech.glide.load.resource.file.FileToStreamDecoder
-import com.caverock.androidsvg.SVG
-import java.io.InputStream
 
 class BalanceBanksAdapter(private var context: Context,
                           private var banks: ArrayList<BalanceBankItemViewModel>,
@@ -43,26 +33,10 @@ class BalanceBanksAdapter(private var context: Context,
             binding.item = item
             binding.executePendingBindings()
 
-            if (item.bank.logoUrl.endsWith(".svg")) {
-                Glide.with(context)
-                    .using(Glide.buildStreamModelLoader(Uri::class.java, context), InputStream::class.java)
-                    .from(Uri::class.java)
-                    .`as`(SVG::class.java)
-                    .transcode(SvgDrawableTranscoder(), PictureDrawable::class.java)
-                    .sourceEncoder(StreamEncoder())
-                    .cacheDecoder(FileToStreamDecoder(SvgDecoder()))
-                    .decoder(SvgDecoder())
-                    .placeholder(R.drawable.ic_banking)
-                    .listener(SvgSoftwareLayerSetter<Uri>())
-                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                    .load(Uri.parse(item.bank.logoUrl))
-                    .into(binding.imageView)
-            } else {
-                Glide.with(context)
-                    .load(item.bank.logoUrl)
-                    .placeholder(R.drawable.ic_banking)
-                    .into(binding.imageView)
-            }
+            Glide.with(context)
+                .load(item.bank.pngLogoUrl)
+                .placeholder(R.drawable.ic_banking)
+                .into(binding.imageView)
         }
 
     }
